09fa8591ad27919cd8a74febe18dd58f4efa047b
selfstudymaster <webselfmaster@gmail.com>
2021-03-09 08:50:27 +0900

    first commit

---
+++ main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+//chapter05
+//list05-01
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("テスト");
+
+	}
+}
+
+
+// 雛形
+// list05-
+//public class Main {
+//
+//	public static void main(String[] args) {
+//
+//
+//	}
+//}
